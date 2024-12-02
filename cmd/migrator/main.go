2d3f7bdb8a6097ebdae3d195054671c7ad971cee
Дамир Барашев <barashev-01@mail.ru>
2024-12-02 11:22:59 +0300

    feat: added postgres conn

--- cmd/migrator/main.go
+++ cmd/migrator/main.go
@@ -6,25 +6,61 @@ import (
 	"fmt"
 
 	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/database/sqlite3"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
-func main() {
-	var storagePath, migrationsPath, migrationsTable string
+const (
+	Postgres string = "postgres"
+	SQLite          = "sqlite"
+)
 
+func main() {
+	var storageType, storagePath, migrationsPath, migrationsTable string
+
+	flag.StringVar(&storageType, "storage-type", "sqlite", "type of storage")
 	flag.StringVar(&storagePath, "storage-path", "", "path to storage")
 	flag.StringVar(&migrationsPath, "migrations-path", "", "path to migrations")
 	flag.StringVar(&migrationsTable, "migrations-table", "migrations", "name of migrations table")
 	flag.Parse()
 
-	if storagePath == "" {
-		panic("storage-path is required")
-	}
 	if migrationsPath == "" {
 		panic("migrations-path is required")
 	}
 
+	if storageType == Postgres {
+		applyMigrationsPostgres(storagePath, migrationsPath, migrationsTable)
+	} else if storageType == SQLite {
+		applyMigrationsSQLite(storagePath, migrationsPath, migrationsTable)
+	} else {
+		panic(fmt.Sprintf("unsupported storage type: %s", storageType))
+	}
+}
+
+func applyMigrationsPostgres(storagePath, migrationsPath, migrationsTable string) {
+	m, err := migrate.New(
+		"file://"+migrationsPath,
+		fmt.Sprintf("postgres://%s?x-migrations-table=%s", storagePath, migrationsTable),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	if err := m.Up(); err != nil {
+		if errors.Is(err, migrate.ErrNoChange) {
+			fmt.Println("no migrations to apply")
+
+			return
+		}
+
+		panic(err)
+	}
+
+	fmt.Println("migrations applied")
+}
+
+func applyMigrationsSQLite(storagePath, migrationsPath, migrationsTable string) {
 	m, err := migrate.New(
 		"file://"+migrationsPath,
 		fmt.Sprintf("sqlite3://%s?x-migrations-table=%s", storagePath, migrationsTable),
--- go.mod
+++ go.mod
@@ -25,6 +25,7 @@ require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
